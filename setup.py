a3887a0a7ee20442e1a3cc0c485bb5b3941b2305
Mark Ferraretto <mark@ferraretto.com>
2020-04-17 01:56:27 -0400

    Generic motion - family law

---
+++ setup.py
@@ -0,0 +1,60 @@
+import os
+import sys
+from setuptools import setup, find_packages
+from fnmatch import fnmatchcase
+from distutils.util import convert_path
+
+standard_exclude = ('*.pyc', '*~', '.*', '*.bak', '*.swp*')
+standard_exclude_directories = ('.*', 'CVS', '_darcs', './build', './dist', 'EGG-INFO', '*.egg-info')
+def find_package_data(where='.', package='', exclude=standard_exclude, exclude_directories=standard_exclude_directories):
+    out = {}
+    stack = [(convert_path(where), '', package)]
+    while stack:
+        where, prefix, package = stack.pop(0)
+        for name in os.listdir(where):
+            fn = os.path.join(where, name)
+            if os.path.isdir(fn):
+                bad_name = False
+                for pattern in exclude_directories:
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        break
+                if bad_name:
+                    continue
+                if os.path.isfile(os.path.join(fn, '__init__.py')):
+                    if not package:
+                        new_package = name
+                    else:
+                        new_package = package + '.' + name
+                        stack.append((fn, '', new_package))
+                else:
+                    stack.append((fn, prefix + name + '/', package))
+            else:
+                bad_name = False
+                for pattern in exclude:
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        break
+                if bad_name:
+                    continue
+                out.setdefault(package, []).append(prefix+name)
+    return out
+
+setup(name='docassemble.MJFprocessedforms',
+      version='0.0.1',
+      description=('A docassemble extension.'),
+      long_description='Store forms that I am processing in this package.\r\n\r\n### Instructions\r\n\r\n1. Get the original form off Trello and copy into playground\r\n1. Get related PDF off Trello and copy into Templates\r\n2. Save YML and PDF to this package\r\n3. Push to GitHub\r\n1. Work on the form\r\n1. Push changes to Github\r\n\r\nIndicate in comments or with a tag when form is ready to release.',
+      long_description_content_type='text/markdown',
+      author='Mark Ferraretto',
+      author_email='mark@ferraretto.com',
+      license='The MIT License (MIT)',
+      url='https://docassemble.org',
+      packages=find_packages(),
+      namespace_packages=['docassemble'],
+      install_requires=[],
+      zip_safe=False,
+      package_data=find_package_data(where='docassemble/MJFprocessedforms/', package='docassemble.MJFprocessedforms'),
+     )
+
